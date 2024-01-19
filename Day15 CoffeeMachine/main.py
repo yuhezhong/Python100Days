c1a8fea0b64911f47469f024d88f0d977cfd1ab3
yuhe <bigzhong0820@foxmail.com>
2024-01-19 17:18:20 +0800

    domo

--- Day15 CoffeeMachine/main.py
+++ Day15 CoffeeMachine/main.py
@@ -1,49 +1,49 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+import menu
 
-profit = 0
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
 
-# What would you like? (espresso/latte/cappuccino): espresso
-# Please insert coins.
-# how many quarters?: 4
-# how many dimes?: 5
-# how many nickles?: 0
-# how many pennies?: 0
-# Here is $0.0 in change.
-# Here is your espresso ☕️. Enjoy!
-# What would you like? (espresso/latte/cappuccino): latte
-# Please insert coins.
-# how many quarters?: 8
-# how many dimes?: 5
-# how many nickles?: 0
-# how many pennies?: 0
-# Here is $0.0 in change.
-# Here is your latte ☕️. Enjoy!
+def order():
+    user_order = input("What would you like? (espresso/latte/cappuccino): ")
+    if user_order == 'espresso':
+        cost = menu.MENU['espresso']['cost']
+        coffee = 'espresso'
+    elif user_order == 'latte':
+        cost = menu.MENU['latte']['cost']
+        coffee = 'latte'
+    elif user_order == 'cappuccino':
+        cost = menu.MENU['cappuccino']['cost']
+        coffee = 'cappuccino'
+    elif user_order == 'report':
+        print(menu.resources)
+    elif user_order == 'off':
+        return 0
+    else:
+        print('Invalid input!')
+    return cost, coffee
+
+
+def money():
+    print("Please insert coins.")
+    money_spent = 0
+    quarter = input("how many quarters? ")
+    money_spent += int(quarter) * 0.25
+    dime = input("how many dimes? ")
+    money_spent += int(dime) * 0.10
+    nickle = input("how many nickles? ")
+    money_spent += int(nickle) * 0.05
+    penny = input("how many pennies? ")
+    money_spent += int(penny) * 0.01
+    return money_spent
+
+
+def compare_money(total_cost, money, coffee):
+    if total_cost > money:
+        print("Sorry that's not enough money. Money refunded.")
+    else:
+        print(f"Here is ${money - total_cost} in change.\nHere is your {coffee} ☕️. Enjoy!")
+
+
+result = order()
+total_cost, coffee = result
+
+money = money()
+compare_money(total_cost, money, coffee)
---
+++ Day15 CoffeeMachine/menu.py
@@ -0,0 +1,49 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+profit = 0
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+# What would you like? (espresso/latte/cappuccino): espresso
+# Please insert coins.
+# how many quarters?: 4
+# how many dimes?: 5
+# how many nickles?: 0
+# how many pennies?: 0
+# Here is $0.0 in change.
+# Here is your espresso ☕️. Enjoy!
+# What would you like? (espresso/latte/cappuccino): latte
+# Please insert coins.
+# how many quarters?: 8
+# how many dimes?: 5
+# how many nickles?: 0
+# how many pennies?: 0
+# Here is $0.0 in change.
+# Here is your latte ☕️. Enjoy!
