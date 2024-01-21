0ede1d786a59af99f74c1e8d614a523b15ed1465
yuhe <bigzhong0820@foxmail.com>
2024-01-21 16:39:50 +0800

    OOP try

---
+++ Day16 OOP/main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+bunny = Turtle()
+print(bunny)
+bunny.shape('turtle')
+bunny.color('black')
+bunny.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
