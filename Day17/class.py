330394e23008d9bb9328ff684982fa8db8c9fe0a
yuhe <bigzhong0820@foxmail.com>
2024-02-16 17:16:23 +0800

    add

--- Day17/class.py
+++ Day17/class.py
@@ -1,3 +1,13 @@
 class User:
-    pass
+    def __init__(self):
+        print('hi, ')
 
+
+user_1 = User()
+user_1.name = 'bunny'
+
+user_2 = User()
+user_2.username = 'yuhe'
+
+print(user_1.name
+      , user_2.username)
